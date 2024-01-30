0e43b7d6f3db7d3d2afc47df8d74978ae62fc9a9
maitetalavera <mtalavera001@student.franciscan.edu>
2024-01-30 11:53:47 -0500

    completed m2

--- src/m2-defining_functions.py
+++ src/m2-defining_functions.py
@@ -1,5 +1,5 @@
 ###############################################################################
-# TODO: 1. (2 pts)
+# DONE: 1. (2 pts)
 #   First, let's define a function. Below this _TODO_, define a function named
 #       hello()
 #   that greets the user. It can say something like
@@ -8,9 +8,10 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
-
+def hello():
+    print("Hello! What a beautiful day we're having!")
 ###############################################################################
-# TODO: 2. (1 pt)
+# DONE: 2. (1 pt)
 #   Now, we need to tell that function to run (in other words, tell it to do
 #   what we have defined it to do).
 #
@@ -20,9 +21,9 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
-
+hello()
 ###############################################################################
-# TODO: 3. (3 pts)
+# DONE: 3. (3 pts)
 #   Now, for practice, let's do that again!
 #
 #   Define a function called
@@ -33,3 +34,6 @@
 #
 #   Once you have done this, then change the above _TODO_ to DONE.
 ###############################################################################
+def goodbye():
+    print("Goodbye! Hope to see you soon.")
+goodbye()
